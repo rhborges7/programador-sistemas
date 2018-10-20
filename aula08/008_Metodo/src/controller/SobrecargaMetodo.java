76089787415b40a19d77ea7f8f129a001b55ad93
Rhuan Borges <44046958+rhborges7@users.noreply.github.com>
2018-10-19 21:40:18 -0300

    Add files via upload

---
+++ aula08/008_Metodo/src/controller/SobrecargaMetodo.java
@@ -0,0 +1,32 @@
+package controller;
+
+public class SobrecargaMetodo {
+	int idade;
+	String nome;
+
+	public static void main(String[] args) {
+		SobrecargaMetodo scm = new SobrecargaMetodo();
+		scm.cadastrarPessoa(38);
+		scm.cadastrarPessoa(22);
+		scm.cadastrarPessoa("Michael Ferreira");
+		scm.cadastrarPessoa(35, "Fabrício Gomes");
+	}
+	
+	public void cadastrarPessoa (int valor) {
+		idade = valor;
+		System.out.println("Idade: "+ idade);
+	}
+	
+	public void cadastrarPessoa (String valor) {
+		nome = valor;
+		System.out.println("Nome:"+nome);
+	}
+	
+	public void cadastrarPessoa (int valor1, String valor2) {
+		idade = valor1;
+		nome = valor2;
+		System.out.println("Idade: "+ idade + " - Nome: "+ nome);
+	
+	}
+
+}
