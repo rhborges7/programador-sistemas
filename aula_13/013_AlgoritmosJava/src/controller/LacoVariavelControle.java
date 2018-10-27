ec2b3de80e29bc69e944afc73d798f165f647bda
Rhuan Borges <44046958+rhborges7@users.noreply.github.com>
2018-10-26 21:18:58 -0300

    Add files via upload

---
+++ aula_13/013_AlgoritmosJava/src/controller/LacoVariavelControle.java
@@ -0,0 +1,29 @@
+package controller;
+
+import util.Teclado;
+
+public class LacoVariavelControle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int r,x,i;
+		
+		for (i=1; i<=5; i++) {
+		x = Teclado.lerInt("Digite um Numero");
+		r = x * 3;
+		System.out.println(r);
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
