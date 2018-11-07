3bc5b91ac5e21ccf1dfac7292986cea87864cad6
Rhuan Borges <44046958+rhborges7@users.noreply.github.com>
2018-11-07 14:05:25 -0200

    Add files via upload

---
+++ aula_19/019_Interface/src/model/Usuario.java
@@ -0,0 +1,48 @@
+package model;
+
+public abstract class Usuario {
+	
+		private String login;
+		private String senha;
+		private int tipo;
+
+		public String getLogin() {
+			return login;
+		}
+
+		public void setLogin(String login) {
+			this.login = login;
+		}
+
+		public String getSenha() {
+			return senha;
+		}
+
+		public void setSenha(String senha) {
+			this.senha = senha;
+		}
+
+		public int getTipo() {
+			return tipo;
+		}
+
+		public void setTipo(int tipo) {
+			this.tipo = tipo;
+		}
+
+		public Usuario() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+
+		public Usuario(String login, String senha, int tipo) {
+			super();
+			this.login = login;
+			this.senha = senha;
+			this.tipo = tipo;
+		}
+
+		public abstract void mostrar();
+	}
+
+
